dcd73fabc4b5d773b1af71a3573bffee3e72088d
kubrasayitass <145451189+kubrasayitass@users.noreply.github.com>
2023-10-17 15:43:01 +0300

    Add files via upload

---
+++ bolum1odev4/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace bolum1odev4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rb1 = new System.Windows.Forms.RadioButton();
+            this.rb2 = new System.Windows.Forms.RadioButton();
+            this.btnUygula = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rb1
+            // 
+            this.rb1.AutoSize = true;
+            this.rb1.Location = new System.Drawing.Point(308, 73);
+            this.rb1.Name = "rb1";
+            this.rb1.Size = new System.Drawing.Size(78, 17);
+            this.rb1.TabIndex = 0;
+            this.rb1.TabStop = true;
+            this.rb1.Text = "Dark Mode";
+            this.rb1.UseVisualStyleBackColor = true;
+            // 
+            // rb2
+            // 
+            this.rb2.AutoSize = true;
+            this.rb2.Location = new System.Drawing.Point(308, 96);
+            this.rb2.Name = "rb2";
+            this.rb2.Size = new System.Drawing.Size(78, 17);
+            this.rb2.TabIndex = 1;
+            this.rb2.TabStop = true;
+            this.rb2.Text = "Light Mode";
+            this.rb2.UseVisualStyleBackColor = true;
+            // 
+            // btnUygula
+            // 
+            this.btnUygula.Location = new System.Drawing.Point(225, 131);
+            this.btnUygula.Name = "btnUygula";
+            this.btnUygula.Size = new System.Drawing.Size(258, 35);
+            this.btnUygula.TabIndex = 2;
+            this.btnUygula.Text = "uygula";
+            this.btnUygula.UseVisualStyleBackColor = true;
+            this.btnUygula.Click += new System.EventHandler(this.btnUygula_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnUygula);
+            this.Controls.Add(this.rb2);
+            this.Controls.Add(this.rb1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rb1;
+        private System.Windows.Forms.RadioButton rb2;
+        private System.Windows.Forms.Button btnUygula;
+    }
+}
+
